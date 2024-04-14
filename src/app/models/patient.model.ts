29d35beab304850549325ff504d81fdd50ee5489
Enes Cicek <bmenescicek@hotmail.com>
2024-04-14 23:30:46 +0300

    Initial Commit

---
+++ src/app/models/patient.model.ts
@@ -0,0 +1,10 @@
+export default class PatientModel {
+    id:string="";
+    firstName:string="";
+    lastName:string="";
+    fullName:string="";
+    city:string="";
+    town:string="";
+    fullAddress:string="";
+    identityNumber:string="";
+};
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ResultModel } from '../models/result.model';
+import { api } from '../constants';
+import { ErrorService } from './error.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  token:string="";
+  constructor(private httpClient: HttpClient,
+    private error: ErrorService
+  ) { 
+    if(localStorage.getItem("token")){
+      this.token=localStorage.getItem("token") ?? "";
+    }
+  }
+
+  post<T>(apiUrl: string, body: any, callBack:(res:ResultModel<T>)=> void, errCallBack?:((err:HttpErrorResponse)=>void)) {
+    this.httpClient.post<ResultModel<T>>(`${api}/${apiUrl}`, body, {
+      headers:{
+        "Authorization":"Bearer " + this.token
+      }
+    })
+      .subscribe({
+        next: (res => {
+            callBack(res);
+        }),
+        error: ((err: HttpErrorResponse) => {
+          this.error.errorHandler(err);
+          if(errCallBack!==undefined){
+            errCallBack(err);
+          }
+        })
+      });
+  }
+}
